57f498fcbfb7ae5207784a4d38f148c5a72c1660
ahmadihya9860 <Ahmadihya9860@gmail.com>
2025-11-10 20:27:47 +0100

    the first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("learn flutter step by step"),
+        backgroundColor: Colors.blue,
+      ),
+      backgroundColor: Colors.blue[999],
+      body: Center(
+        child: Image(
+          image: AssetImage("images/imag.jpg"),
+        ),
+      )
+    ),
+  ));
+}
